4b68cb9933c9eae7bc0f1d914a6467b61b4b5109
Sirius147 <91835961+Sirius147@users.noreply.github.com>
2024-08-05 22:35:39 +0900

    [Gold IV] Title: N-Queen, Time: 29056 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,30 +1,25 @@
 import sys
 input = sys.stdin.readline
-
+sys.setrecursionlimit(10**6)
 n = int(input())
 
 colStates = [True for _ in range(n)]
-cnt = 0; track = []
+digonals,antiDigonals = set(), set()
+cnt = 0
 
-def backtracking(row:int, numQ:int):
+def backtracking(row:int):
     global cnt
-    
-    def tracker(i:int, j:int):
-        for x,y in track:
-            if abs((j-y)/(i-x)) == 1: return False
-        return True
 
-    if numQ == n:
+    if row == n:
         cnt += 1
         return
     
     for j in range(0,n):
-        if colStates[j] and tracker(row,j):    
-            track.append((row,j))
-            colStates[j] = False
-            backtracking(row+1,numQ+1)
-            colStates[j] = True
-            track.pop()
+        if colStates[j] and ((row+j) not in digonals) and ((row - j) not in antiDigonals):
+            colStates[j] = False; digonals.add(row+j); antiDigonals.add(row-j)
+            backtracking(row+1)
+            colStates[j] = True; digonals.remove(row+j); antiDigonals.remove(row-j)
+            
 
-backtracking(0,0)
+backtracking(0)
 print(cnt)
