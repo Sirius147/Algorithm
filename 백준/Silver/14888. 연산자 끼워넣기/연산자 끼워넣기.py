618624bece6167aa70d6a74f562b5cf9a23f677a
Sirius147 <91835961+Sirius147@users.noreply.github.com>
2024-08-21 17:12:00 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 5396 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,23 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-lEngth = int(input())
-seq = list(map(int,input().split()))
-
-opsBucket = list(map(int,input().split()))
-ops = ""; opsDict = {0:'+', 1:'-', 2:'*', 3:'/'}
-for i in range(len(opsBucket)):
-    ops += opsDict[i]*opsBucket[i]
-ops = list(ops)
-
-opsStates = [False for _ in range(len(ops))]
-
+opsDict = {0:'+', 1:'-', 2:'*', 3:'/'}
 results = set()
 
-def backtracking(level:int,preVal:int):
-    if level == len(seq):
-        results.add(preVal)
-        return
+def solve():
+    global results
+    
+    lEngth = int(input())
+    seq = list(map(int,input().split()))
+
+    opsBucket = list(map(int,input().split()))
+    ops = ""
+    for i in range(len(opsBucket)):
+        ops += opsDict[i]*opsBucket[i]
+    ops = list(ops)
+
+    opsStates = [False for _ in range(len(ops))]
+    
     def operation(op,oper1,oper2)->int:
         if op == '+':
             return oper1 + oper2
@@ -31,15 +31,23 @@ def backtracking(level:int,preVal:int):
                 tmp = oper1 // oper2
                 return -1 * tmp
             return oper1 // oper2
+    
+    def backtracking(level:int,preVal:int):
+        if level == lEngth:
+            results.add(preVal)
+            return
         
+        for i in range(len(ops)):
+            if opsStates[i] == False:
+                newVal = operation(ops[i],preVal,seq[level])
+                opsStates[i] = True
+                backtracking(level+1,newVal)
+                opsStates[i] = False
 
-    for i in range(len(ops)):
-        if opsStates[i] == False:
-            newVal = operation(ops[i],preVal,seq[level])
-            opsStates[i] = True
-            backtracking(level+1,newVal)
-            opsStates[i] = False
+    backtracking(1,seq[0])
+    
 
 
-backtracking(1,seq[0])
-print(max(results),min(results),sep = '\n')
+if __name__ == "__main__":
+    solve()
+    print(max(results),min(results),sep = '\n')
