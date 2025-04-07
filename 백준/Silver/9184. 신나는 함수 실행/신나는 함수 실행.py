c0efa86587be80c65559fcf0ce3b4ec11c7e793f
sirius <91835961+Sirius147@users.noreply.github.com>
2025-04-07 14:43:07 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 148 ms, Memory: 111472 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,24 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def w(a, b, c):
-    if a <= 0 or b<= 0 or c<=0:
-        return 1
-    if a > 20 or b > 20 or c > 20:
-        return w(20, 20, 20)
-    if dp[a][b][c]:
-        return dp[a][b][c]
-    if a<b<c:
-        dp[a][b][c] = w(a,b,c-1)+w(a,b-1,c-1)-w(a, b-1, c)
-        return dp[a][b][c]
-    dp[a][b][c] = w(a-1, b, c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1)
-    return dp[a][b][c]
+dp = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
 
-dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
-# 0~20까지므로
+for i in range(21):
+    for j in range(21):
+        for k in range(21):
+            if i == 0 or j == 0 or k == 0:
+                dp[i][j][k] = 1
+            elif i < j and j < k:
+                dp[i][j][k] = dp[i][j][k-1] + dp[i][j-1][k-1] - dp[i][j-1][k]
+            else: dp[i][j][k] = dp[i-1][j][k] + dp[i-1][j-1][k] + dp[i-1][j][k-1] - dp[i-1][j-1][k-1]
 
-while 1:
-    a, b, c = map(int, input().split())
-    if a==-1 and b==-1 and c==-1:
-        break
-    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+
+while True:
+    a,b,c = map(int,input().split())
+    if a == -1 and b == -1 and c == -1: break
+    elif a <= 0 or b <= 0 or c <= 0: print(f"w({a}, {b}, {c}) = 1")
+    elif a > 20 or b > 20 or c > 20: print(f"w({a}, {b}, {c}) = {dp[20][20][20]}")
+    else: print(f"w({a}, {b}, {c}) = {dp[a][b][c]}")
