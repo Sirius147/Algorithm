3e224502f1f04442631744030181a999923632ed
sirius <91835961+Sirius147@users.noreply.github.com>
2025-11-24 23:39:37 +0900

    [level 2] Title: 후보키, Time: 3.67 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+def solution(relation):
+    answer = 0
+
+
+    candikeys = []
+    rows = len(relation)
+    cols = len(relation[0])
+    colIdxs = {i for i in range(cols)}
+    
+    for i in range(1,cols + 1):
+        if len(colIdxs) < i:
+            break
+
+        for cs in combinations(colIdxs, i):
+            
+            minimality = True
+            
+            for kys in candikeys:
+                if set(cs) & kys == kys: minimality = False
+                    
+            if not minimality: continue
+            
+            tpSet = set()
+            
+            for j in range(rows):
+                tp = ""
+                for k in range(i):
+                    tp += relation[j][cs[k]]
+                tpSet.add(tp)
+                
+            if len(tpSet) == rows:
+                answer += 1
+                candikeys.append(set(cs))
+
+
+    return answer
