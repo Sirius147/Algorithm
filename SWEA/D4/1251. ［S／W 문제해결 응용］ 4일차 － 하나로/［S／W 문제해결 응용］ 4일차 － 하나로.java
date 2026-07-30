e1f9c8948934231a0946678d01cad8d158139070
sirius <91835961+Sirius147@users.noreply.github.com>
2026-07-31 01:39:55 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,026 ms, Memory: 99,124 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -15,18 +15,18 @@ public class Solution {
 	public static int[] parent;
 	public static double ans = 0.0;
 	
-	public static int[] find(double d, int depth) {
+	public static int find(double d) {
 		if (parent[(int) d] == (int) d) {
-			return new int[] {(int) d, depth};
+			return (int) d;
 		} 
-		return find(parent[(int) d], depth+1);
+		return parent[(int) d] = find(parent[(int) d]);
 	}
 	
-	public static void union(int[] a, int[] b) {
-		if (a[1] < b[1]) {
-			parent[a[0]] = b[0];
+	public static void union(int a, int b) {
+		if (a < b) {
+			parent[a] = b;
 		} else {
-			parent[b[0]] = a[0];
+			parent[b] = a;
 		}
 	}
 	
@@ -71,8 +71,8 @@ public class Solution {
 			int edgeCnt = 0;
 			for (double[] d : graph) {
 				if (edgeCnt == n-1) break;
-				int[] x = find(d[0],1), y = find(d[1],1);
-				if (x[0] != y[0]) {
+				int x = find(d[0]), y = find(d[1]);
+				if (x != y) {
 					union(x,y);
 					ans += d[2];
 					edgeCnt++;
